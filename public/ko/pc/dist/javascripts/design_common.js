58bdde5cbd83ea965c8ee30aa8717db4cf22a9d7
minju <amilk5u@naver.com>
2021-12-28 18:31:09 +0900

    hysoung commit

--- public/ko/pc/dist/javascripts/design_common.js
+++ public/ko/pc/dist/javascripts/design_common.js
@@ -16,6 +16,15 @@ var $gnb = $("#gnb"),
     $closeBtn = $(".btn_close"),
     $menu = $("#menu");
 
+var $tabMenu = $(".tab_menu"),
+    $tabList = $tabMenu.find("li"),
+    $tabConts = $(".tab_contents");
+
+var $popupBtn = $(".btn_popup"),
+    $popupBox = $(".popup"),
+    $popupBg = $(".dimmed"),
+    $closePopupBtn = $(".btn_popup_close");
+
 $window.load(function () {
     var _this =  $(this);
     winW = _this.width();
@@ -54,10 +63,152 @@ function explore() {
 
 }
 function layout() {
+    //form script
+    $(".form_type01 input").on({
+        focus : function(){
+            $(this).addClass('validField');
+        },
+        blur : function(){
+            if($(this).val() == ""){
+                $(this).removeClass('validField');
+            }
+        }
+    });
 
+    // GNB
+    $gnb.find(".menu").dbNaviTwoDepthSwap({
+        motionSpeed:500,
+        motionType:'slide'
+    });
+
+    // popup
+    $popupBtn.click(function(){
+        var _seletePopup = $(this).data("popup");
+        $("."+_seletePopup).css({"display":"flex"});
+    });
+    $closePopupBtn.click(function(){
+        $popupBox.fadeOut();
+    });
+    $popupBg.click(function(){
+        $popupBox.fadeOut();
+    });
 }
 function main() {
+    var visualSlide = new Swiper(".visual_slide", {
+        effect: "fade",
+        loop: true,
+        autoplay: {
+            delay: 7000,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: ".visual_next_btn",
+            prevEl: ".visual_prev_btn",
+        },
+    });
 
+    var retailSlide = new Swiper(".retail_slide", {
+        slidesPerView: 1,
+        breakpoints: {
+            768: {
+                slidesPerView: 2
+            },
+            1024: {
+                slidesPerView: 3
+            },
+            1200: {
+                slidesPerView: 4
+            }
+        }
+    });
+
+    var financialSlide = new Swiper(".financial_slide", {
+        slidesPerView: 1,
+        breakpoints: {
+            768: {
+                slidesPerView: 2
+            },
+            1024: {
+                slidesPerView: 3
+            },
+            1200: {
+                slidesPerView: 4
+            }
+        }
+    });
+
+    $tabList.click(function(){
+        $tabList.removeClass("on");
+        $tabConts.find("> div").hide();
+        var activeTab = $(this).data("tab");
+        $(this).addClass("on");
+        $tabConts.find("."+activeTab).show();
+    });
+
+    $(".thumb_slide01 button").click(function(){
+        $(".thumb_slide01 button").removeClass("active");
+        $(this).addClass("active");
+    });
+    $(".thumb_slide02 button").click(function(){
+        $(".thumb_slide02 button").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    var thumbSlide01 = new Swiper(".thumb_slide01", {
+        slidesPerView: 2,
+        spaceBetween: 25,
+        navigation: {
+            nextEl: ".thumb_slide_next01",
+            prevEl: ".thumb_slide_prev01",
+        },
+        observer: true,
+        observeParents: true,
+        breakpoints: {
+            768: {
+                slidesPerView: 2
+            },
+            1024: {
+                slidesPerView: 4
+            },
+            1200: {
+                slidesPerView: 6
+            }
+        }
+    });
+    var partsSlide01 = new Swiper(".parts_slide01", {
+        slidesPerView: "auto",
+        // spaceBetween: 10,
+        observer: true,
+        observeParents: true
+    });
+
+    var thumbSlide02 = new Swiper(".thumb_slide02", {
+        slidesPerView: 2,
+        spaceBetween: 25,
+        navigation: {
+            nextEl: ".thumb_slide_next02",
+            prevEl: ".thumb_slide_prev02",
+        },
+        observer: true,
+        observeParents: true,
+        breakpoints: {
+            768: {
+                slidesPerView: 2
+            },
+            1024: {
+                slidesPerView: 4
+            },
+            1200: {
+                slidesPerView: 6
+            }
+        }
+    });
+    var partsSlide02 = new Swiper(".parts_slide02", {
+        slidesPerView: "auto",
+        // spaceBetween: 10,
+        observer: true,
+        observeParents: true
+    });
 }
 function member() {
 
